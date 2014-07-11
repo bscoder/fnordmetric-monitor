3ec827c751595727f4370c89d96c2527a155b838
Sergey Boltushkin <sergey.boltushkin@gmail.com>
2014-07-11 17:23:38 +0400

    Fix for new version Remailer

--- lib/fnordmetric/monitor/contact_email.rb
+++ lib/fnordmetric/monitor/contact_email.rb
@@ -6,12 +6,18 @@ class FnordMetric::ContactEmail
   end
 
   def send_message(message_text, opts={})
-    connection = opts[:smtp_connection] ||
-      Remailer::Connection.open(opts[:smtp_host] || 'localhost', :debug => STDERR)
+    if opts[:smtp_connection] then
+      connection = opts[:smtp_connection]
+      connection_new = false
+    else
+      connection = Remailer::SMTP::Client.open(opts[:smtp_host] || 'localhost', :debug => STDERR)
+      connection_new = true
+    end
 
     connection.send_email(
-                            opts[:smtp_from] || 'noreply@localhost',
-                            @email,
-                            message_text)
+                          opts[:smtp_from] || 'noreply@localhost',
+                          @email,
+                          message_text)
+    connection.close_when_complete! if connection_new
   end
 end
